7d036e3e751286237a312bac3db04a52a34f1c28
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-02-27 00:30:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -52,34 +52,34 @@ client = pyrogram.Client(
 def main():
     with client:
         while True:
-            edit_text = "<b>👾 Our Bots' Status (Updating Every 45 Minutes)</b>\n\n"
+            edit_text = "<b>👾 @DamienSoukara Our Bot's Status (Updating Every 2 Hours)</b>\n\n📜 BOTS :\n\n"
 
             for bot in BOTS:
                 snt = client.send_message(bot, "/start")
-                time.sleep(5)
+                time.sleep(7)
                 msg = client.get_history(bot, 1)[0]
 
                 if snt.message_id == msg.message_id:
-                    edit_text += f"❌ - @{bot} : DOWN\n\n"
+                    edit_text += f"❌ - @{bot} : DOWN\n"
                     client.send_message(
                         BOT_OWNER,
                         f"❌ - @{bot} IS DOWN !"
                     )
                 else:
-                    edit_text += f"✅ - @{bot} : UP\n\n"
+                    edit_text += f"✅ - @{bot} : UP\n"
 
                 client.read_history(bot)
 
             utc_now = datetime.datetime.utcnow()
 
-            edit_text += f"⏱ <i>LAST UPDATE : {str(utc_now)} UTC</i>"
+            edit_text += f"\n🌍 <b>LAST UPDATE :</b>\n{str(utc_now)} UTC"
 
             client.edit_message_text(
                 UPDATE_CHANNEL,
                 SATUS_MESSAGE_MESSAGE_ID,
                 edit_text
             )
-            time.sleep(45 * 60)
+            time.sleep(120 * 60)
 
 
 main()
