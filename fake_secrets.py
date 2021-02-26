b38a2cb05f8ef80a8fba1d3929fdabad1f809a7f
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-02-26 23:50:39 +0100

    Create fake_secrets.py

---
+++ fake_secrets.py
@@ -0,0 +1,7 @@
+SESSION_STRING = "my_account"
+API_ID = 1234567
+API_HASH = "abcdefghijklmnop123456789"
+BOT_OWNER = "AmineSoukara"
+BOTS = "ListSeriesRobot AppsBdarijaRobot MoviesBdarijaRobot ShareMyPostRobot FileSharerRobot XCovid19Bot ReFileRobot MarsRobot PicsEditorBot ToTelegraphBot ForceSubscribeRobot ForzarSuscripcionRobot ForceSubscribeArBot ForceSubscribeItaBot"
+UPDATE_CAHNNEL = "DamienOt"
+SATUS_MESSAGE_MESSAGE_ID = 471
