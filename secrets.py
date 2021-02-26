5b85a1d6b8326bc68d0ef25f1ea5e5f375b6056b
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-02-27 00:03:08 +0100

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -3,5 +3,5 @@ API_ID = 1234567
 API_HASH = "abcdefghijklmnop123456789"
 BOT_OWNER = "AmineSoukara"
 BOTS = "ListSeriesRobot AppsBdarijaRobot MoviesBdarijaRobot ShareMyPostRobot FileSharerRobot XCovid19Bot ReFileRobot MarsRobot PicsEditorBot ToTelegraphBot ForceSubscribeRobot ForzarSuscripcionRobot ForceSubscribeArBot ForceSubscribeItaBot"
-UPDATE_CAHNNEL = "DamienOt"
+UPDATE_CHANNEL = "DamienOt"
 SATUS_MESSAGE_MESSAGE_ID = 471
