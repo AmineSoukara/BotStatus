1504986d553e0a5d336a6f8654b966089996543f
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2021-04-09 03:56:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,53 +6,60 @@ import datetime
 import pytz
 import pyrogram
 
-# api strings from my.telegram.org
-api_id = int(os.environ.get("API_ID"))
-api_hash = os.environ.get("API_HASH")
+# Api Strings From my.telegram.org
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
 
-# your session strings
-user_session_string = os.environ.get("SESSION_STRING")
+# Your Session Strings
+SESSION_STRING = os.environ.get("SESSION_STRING")
 
-# your all bots username without '@' with a space 1 to another
-bots = [i.strip() for i in os.environ.get("BOTS").split(' ')]
+# Your Bots Username Without '@' With A Space 1 To Another
+BOTS = [i.strip() for i in os.environ.get("BOTS").split(' ')]
 
-# your username without '@'
-bot_owner = os.environ.get("BOT_OWNER")
+# Your Username Without '@'
+BOT_OWNER = os.environ.get("BOT_OWNER")
 
-# your channel username without '@'
-update_channel = os.environ.get("UPDATE_CHANNEL")
+# Your Channel Username Without '@'
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL")
 
-# message id of your channel bot status message
-status_message_id = int(os.environ.get("STATUS_MESSAGE_ID"))
+# Message Id Of Your Channel Bot Status Message
+STATUS_MESSAGE_ID = int(os.environ.get("STATUS_MESSAGE_ID"))
 
-# time and limits
-timelimit = int(os.environ.get("TIME"))
+# Time & Limits
+TIME = int(os.environ.get("TIME"))
 
-user_client = pyrogram.Client(user_session_string, api_id=api_id, api_hash=api_hash)
+Alty = pyrogram.Client(SESSION_STRING, api_id=API_ID, api_hash=API_HASH)
 
 def main():
-    with user_client:
+    with Alty:
         while True:
-            print("[INFO] Starting To Check uptime..")
-            edit_text = f"<u><b>My Bots Online Status</u></b>\n"
-            for bot in bots:
-                print(f"[INFO] checking @{bot}")
-                snt = user_client.send_message(bot, '/start')
-                time.sleep(30)
-                msg = user_client.get_history(bot, 1)[0]
-                if snt.message_id == msg.message_id:
-                    print(f"[WARNING] @{bot} is down")
-                    edit_text += f"\n🤖 <b>Bot :-</b> <a href='https://telegram.me/{bot}'>{bot}</a>\n<b>⚜ Status :-</b> <code>Offline</code> ❎\n"
-                    user_client.send_message(bot_owner, f"@{bot} status: <code>Down</code>")
+            print("💬 [INFO] Starting To Check Uptime..")
+            TEXT = f"<b>👾 @{UPDATE_CHANNEL} Our Bot's Status (Updating Every  {round(TIME / 60)} Hours Hours)</b>\n\n<b>📜 BOTS :</b>\n\n"
+
+            for bot in BOTS:
+                print(f"💬 [INFO] Checking @{bot}")
+
+                x = Alty.send_message(bot, '/start')
+                time.sleep(15)
+                msg = Alty.get_history(bot, 1)[0]
+
+                if x.message_id == msg.message_id:
+                    print(f"⚠️ [WARNING] @{bot} Is Down")
+                    TEXT += f"❌ - @{bot}\n"
+                    Alty.send_message(BOT_OWNER, f"❌ - @{bot} IS DOWN !")
                 else:
-                    print(f"[INFO] all good with @{bot}")
-                    edit_text += f"\n🤖 <b>Bot :-</b> <a href='https://telegram.me/{bot}'>{bot}</a>\n<b>⚜ Status :-</b> <code>Online</code> ✅\n"
-                user_client.read_history(bot)
+                    print(f"☑ [INFO] All Good With @{bot}")
+                    TEXT += f"✅ - @{bot}\n"
+                Alty.read_history(bot)
+
             utc_now = datetime.datetime.now(pytz.timezone('UTC')).strftime("%I:%M %p %d/%m/%y")
-            edit_text += f"""\n<b>Last checked:</b>\n{str(utc_now)} UTC ⏰\n<code>Updated on every {round(timelimit / 60)} hours</code>"""
-            user_client.edit_message_text(update_channel, status_message_id, text=edit_text, disable_web_page_preview=True, parse_mode="html")
-            print(f"[INFO] everything done! sleeping for {round(timelimit / 60)} hours...")
-            time.sleep(timelimit * 60)
+            ma_now = datetime.datetime.now(pytz.timezone('Africa/Casablanca')).strftime("%d/%m/%y %I:%M:%S %p")
+
+            TEXT += f"\n⏱ <b>LAST UPDATE :</b>\n\n🌎 UTC : {str(utc_now)}\n🇲🇦 MA : {str(ma_now)}"
+
+            Alty.edit_message_text(UPDATE_CHANNEL, STATUS_MESSAGE_ID, text=TEXT, disable_web_page_preview=True, parse_mode="html")
+            print(f"[INFO] Everything Done! Sleeping For {round(TIME / 60)} Hours...")
+            time.sleep(TIME * 60)
 
 
 main()
